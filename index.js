53ddaa0a76d396a1921b59d03ff41441f10879e7
Mark Roberts <marker2911@gmail.com>
2023-01-03 17:17:26 +0000

    Added draw card button

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+//assign variable, leave it undefined for fetch request
+let deckId
+
+// BUTTONS
 const getNewDeckBtn = document.getElementById("new-deck-btn")
+const drawCardsBtn = document.getElementById("draw-cards-btn")
 
-getNewDeckBtn.addEventListener("click", fetchNewDeck)
 
-function fetchNewDeck() {
+getNewDeckBtn.addEventListener("click", getNewDeck)
+drawCardsBtn.addEventListener("click", drawCards)
+
+function getNewDeck() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            deckId = data.deck_id
+            console.log(deckId)
     })
+}
+
+function drawCards() {
+    if (deckId) {
+        fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+            .then(res => res.json())
+            .then (data => {
+                console.log(data.cards)
+            })
+
+    } else {
+        console.log("No deck found")
+    }
 }
